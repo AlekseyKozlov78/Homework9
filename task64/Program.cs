a397f922c0655bf4a8d3a1b7f412a30fbc0bc57c
Aleksey <kozlovav78@mail.ru>
2022-08-20 16:36:10 +0300

    Решение задачи 64

---
+++ task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Задайте значение M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+string FindNatureNumber( int m, int n)
+{
+ if( m == n) return n.ToString();
+   else return m.ToString() + ", " + FindNatureNumber(m+=1, n);
+}
+Console.WriteLine (FindNatureNumber(numberM, numberN));
+
